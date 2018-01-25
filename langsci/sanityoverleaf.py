981f5085bb0d21409a24ae795e693b5703c1f0ea
glottotopia <sebastian.nordhoff@glottotopia.de>
2018-01-25 09:49:47 +0100

    take care of existing repos

--- langsci/sanityoverleaf.py
+++ langsci/sanityoverleaf.py
@@ -1,18 +1,32 @@
 from sanitycheck import LSPDir
 import re
-from git import Repo
+import  git 
 import os
 import sys
+import subprocess
 
     
 if __name__ == "__main__":
-  overleafurl = sys.argv[1]
-  m = re.search('([0-9]{7,}[a-z]+)',overleafurl)
-  overleafID = m.group(1)
-  giturl = "https://git.overleaf.com/"+overleafID
-  gitdir = './'+overleafID    
-  Repo.clone_from(giturl, gitdir)
-  lspdir = LSPDir(os.path.join(gitdir,'chapters'))
-  print "checking %s" % ' '.join([f for f in lspdir.texfiles+lspdir.bibfiles])
-  lspdir.check()
-  lspdir.printErrors()
+    overleafurl = sys.argv[1]
+    m = re.search('([0-9]{7,}[a-z]+)',overleafurl)
+    overleafID = m.group(1)
+    print "overleaf ID found:", overleafID
+    giturl = "https://git.overleaf.com/"+overleafID
+    gitdir = './'+overleafID  
+    print "git repo is ", giturl
+    try:
+        git.Repo.clone_from(giturl, gitdir)
+        print "cloned"
+    except git.exc.GitCommandError:
+        print "repo already in file system. Pulling"        
+        cwd = os.getcwd()
+        print gitdir
+        os.chdir(gitdir)
+        subprocess.call(["git","pull"])
+        subprocess.call("ls")
+        os.chdir(cwd)
+        print "pulled"
+    lspdir = LSPDir(os.path.join(gitdir,'chapters'))
+    print "checking %s" % ' '.join([f for f in lspdir.texfiles+lspdir.bibfiles])
+    lspdir.check()
+    lspdir.printErrors()
