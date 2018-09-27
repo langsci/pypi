e45f139de4b85099a46340a20169e6fb33dbbb79
glottotopia <sebastian.nordhoff@glottotopia.de>
2018-09-27 14:10:43 +0200

    clear __main__ from packages

--- langsci/langsci/fixindex.py
+++ langsci/langsci/fixindex.py
@@ -1,36 +1,32 @@
-import sys
-
-
-import langsci
-print(langsci.__dir__())
+import sys  
 from indextools import processfile
 
-#if __name__ == '__main__':
-"""
-Transform a file main._dx into mainmod._dx, where the underscore represents an index type given in the argument.
+if __name__ == '__main__':
+    """
+    Transform a file main._dx into mainmod._dx, where the underscore represents an index type given in the argument.
 
-Index types are:
-a: author index 
-s: subject index 
-l: language index
+    Index types are:
+    a: author index 
+    s: subject index 
+    l: language index
 
-Examples: 
-> python3 fixindex.py 
-fixes the author index only. Read: main.adx. Write: mainmod.adx
-> python3 fixindex.py a
-fixes the author index only. Read: main.adx. Write: mainmod.adx
-> python3 fixindex.py l
-fixes the language index only. Read: main.ldx. Write: mainmod.ldx
-> python3 fixindex.py lsa
-fixes all three index types. Read: main.adx, main.ldx, main.sdx. Write: mainmod.adx, mainmod.ldx, mainmod.sdx
-"""
-  
-indextypes = 'a'  
-try:
-    indextypes = sys.argv[1]
-except IndexError:
-    pass
-for indextype in indextypes:
-    processfile("main.%sdx" % indextype)
-  
+    Examples: 
+    > python3 fixindex.py 
+    fixes the author index only. Read: main.adx. Write: mainmod.adx
+    > python3 fixindex.py a
+    fixes the author index only. Read: main.adx. Write: mainmod.adx
+    > python3 fixindex.py l
+    fixes the language index only. Read: main.ldx. Write: mainmod.ldx
+    > python3 fixindex.py lsa
+    fixes all three index types. Read: main.adx, main.ldx, main.sdx. Write: mainmod.adx, mainmod.ldx, mainmod.sdx
+    """
+    
+    indextypes = 'a'  
+    try:
+        indextypes = sys.argv[1]
+    except IndexError:
+        pass
+    for indextype in indextypes:
+        processfile("main.%sdx" % indextype)
+    
   
