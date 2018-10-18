ca31459e5422f4a1a1ee367f1c7a18579781202c
glottotopia <sebastian.nordhoff@glottotopia.de>
2018-10-18 09:14:56 +0200

    correct import

--- langsci/wrapperscripts/txt2bib.py
+++ langsci/wrapperscripts/txt2bib.py
@@ -1,8 +1,8 @@
 import sys  
 try:
-    from langscibibtex import Record
+    from bibtools import Record
 except ImportError:
-    from langsci.langscibibtex import Record 
+    from langsci.bibtools import Record 
 
 filename = sys.argv[1]
 lines = open(filename).readlines()
