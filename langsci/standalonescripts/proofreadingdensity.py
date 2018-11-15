6495aabd35cb4961ecf1e587463691bb9cff09b9
Felix Kopecky <kopeckyf@hu-berlin.de>
2018-11-15 10:26:35 +0100

    a simple proofreading density checker

---
+++ langsci/standalonescripts/proofreadingdensity.py
@@ -0,0 +1,25 @@
+import sys
+import json
+import requests
+import numpy
+import matplotlib.pyplot as plt
+
+# let us first obtain the Paperhive document ID related to a LangSci book ID
+lsidrequest = requests.get('https://paperhive.org/api/document-items/by-document/external?type=langsci&id=' + sys.argv[1])
+lsidformated = json.loads(lsidrequest.text)
+# save the ID.
+paperhiveid = lsidformated['documentItems'][0]['id']
+paperhiverequest = requests.get('https://paperhive.org/api/discussions?documentItem=' + paperhiveid)
+paperhiveformated = json.loads(paperhiverequest.text)
+# get the number of pages
+numpages=int(sys.argv[2]) # replace with 
+pages = [] # create empty list for storage of pages
+for d in paperhiveformated['discussions']: # 'd' as in 'discussion'
+    for p in d['target']['selectors']['pdfTextPositions']: # 'p' as in 'page'
+        pages = pages + [p['pageNumber']]
+
+# and now for plotting
+n, bins, patches = plt.hist(pages,numpages)
+plt.ylabel('number of comments')
+plt.xlabel('page number')
+plt.show()
