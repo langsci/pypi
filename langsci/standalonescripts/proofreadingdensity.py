e234bdda433920fa26cd20c4e0c42f9b741313fc
glottotopia <sebastian.nordhoff@glottotopia.de>
2018-11-15 10:58:13 +0100

    save file

--- langsci/standalonescripts/proofreadingdensity.py
+++ langsci/standalonescripts/proofreadingdensity.py
@@ -5,7 +5,8 @@ import numpy
 import matplotlib.pyplot as plt
 
 # let us first obtain the Paperhive document ID related to a LangSci book ID
-lsidrequest = requests.get('https://paperhive.org/api/document-items/by-document/external?type=langsci&id=' + sys.argv[1])
+lsID = sys.argv[1]
+lsidrequest = requests.get('https://paperhive.org/api/document-items/by-document/external?type=langsci&id=' + lsID)
 lsidformated = json.loads(lsidrequest.text)
 # save the ID.
 paperhiveid = lsidformated['documentItems'][0]['id']
@@ -22,5 +23,6 @@ numpages = max(pages) #stop at last page with comment, leaving further pages w/o
 # and now for plotting
 n, bins, patches = plt.hist(pages,numpages)
 plt.ylabel('number of comments')
-plt.xlabel('page number')
-plt.show()
+plt.xlabel('page number') 
+plt.savefig('%s.png'%lsID)
+plt.show() 
