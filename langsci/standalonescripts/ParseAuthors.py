85917d89c675244be81ac9cd61dc7512c2871e9c
mstelling <marten.stelling@uni-goettingen.de>
2020-12-22 17:28:52 +0100

    fixes to ParseAuthors

--- langsci/standalonescripts/ParseAuthors.py
+++ langsci/standalonescripts/ParseAuthors.py
@@ -19,8 +19,7 @@ warnings = []
 
 
 
-
-def append_authors(line):
+def append_authors_old(line):
     authors_dict = {}
     line = line[line.find('\\author')+8:line.rfind('}')]
     line = re.sub(r'\\orcid{.*}','',line)
@@ -36,6 +35,21 @@ def append_authors(line):
 def append_bookauthors(line):
     line = line[line.find('\\author')+8:line.rfind('}')]
     authors_list = (re.split("\\\\and\s|\\\\lastand", line))
+    return(authors_list)
+
+# work in progress. probably doesn't work yet
+def append_authors(line):
+    line = line[line.find('\\author')+8:line.rfind('}')]
+    line = re.sub(r'\\orcid{.*}','',line)
+    authors_list = (re.split("\\\\and\s|\\\\lastand", line))
+    authors_temp = []
+    for item in authors_list:
+        if "\\affiliation" in item:
+            splititem = (re.split("\\\\affiliation{", item))
+            newitem = {"name" : splititem[0], "affiliation":re.sub('}', '', splititem[1])}
+            authors_temp.append(newitem)
+    if authors_temp:
+        authors_list = authors_temp[:]
     return(authors_list)
 
 
@@ -65,13 +79,14 @@ def create_database(id):
         elif "\\author" in line and not "%" in line[0: line.find("\\author")]:
              metadata_dict["bookauthor"] = append_bookauthors(line)
     for line in mainfile.splitlines():
-        if "\\include{chapters/" in line and not "%" in line[0: line.rfind("\\include")]:
+        if "\\include{chapters/" in line and not "%" in line[0: line.find("\\include")]:
             chapternames.append(line[line.find('\\include{chapters/')+18:line.find('}')])
-        elif "\\includepaper{" in line and not "%" in line[0: line.rfind("\\include")]:
+        elif "\\includepaper{" in line and not "%" in line[0: line.find("\\include")]:
              chapternames.append(line[line.find('\\includepaper{chapters')+23:line.find('}')])
     for chaptername in chapternames:
         chapterdict = {"chapterfilename":chaptername}
         chapterauthor= {}
+        chapterauthors_list = []
         chapterfile = requests.get('%s%s%schapters/%s.tex' % (masterurl1, str(id), masterurl2, chaptername)).text
         print("Reading chapter %s" % chaptername)
         for line in chapterfile.splitlines():
@@ -84,9 +99,11 @@ def create_database(id):
                 chaptertitle = (line[line.find('\\title{')+7:line.find('}')])
                 chapterdict["chaptername"] = chaptertitle
             elif "\\author" in line and not "%" in line[0: line.find("chapter")]:
-                chapterdict["author"] = append_authors(line)
+                chapterdict["authors"] = append_authors(line)
 
         chapterdict_list.append(chapterdict)
+
+        #basic logging:
         if "\\{" in chapterdict:
             logfile.write("WARNING: \\{ detected in chapter %s. This will likely break the parsing of that line.")
             print("WARNING: \\{ detected in chapter %s. This will likely break the parsing of that line.")
@@ -100,14 +117,25 @@ def make_jsons(id):
     outputfile.close()
 
   
+  
+def full_table(startid,endid):
+    for id in range(startid,endid+1):
+        print("Processing book %s" % str(id))
+        if os.path.exists("%s.log" % str(id)):
+            os.remove("%s.log" % str(id))
+        print("Logging in %s.log" % str(id))
+        logfile = open("%s.log" % str(id), "a")
+        make_jsons(id)
+        logfile.close()
 
-for id in range(startid,endid+1):
-    print("Processing book %s" % str(id))
-    if os.path.exists("%s.log" % str(id)):
-        os.remove("%s.log")
-    print("Logging in %s.log" % str(id))
-    logfile = open("%s.log" % str(id), "a")
-    make_jsons(id)
-    logfile.close()
+def chapterauthors_table(startid,endid):
+        for id in range(startid,endid+1):
+            print("Processing book %s" % str(id))
+            if os.path.exists("%s.log" % str(id)):
+                os.remove("%s.log")
+            print("Logging in %s.log" % str(id))
+            logfile = open("%s.log" % str(id), "a")
+            
 
 
+full_table(startid,endid)
