df3ece74dcd452d0e22788eb41ad7da382e7b135
glottotopia <sebastian.nordhoff@glottotopia.de>
2022-03-14 21:11:42 +0100

    better treatment of language metadata

--- langsci/standalonescripts/extractgll.py
+++ langsci/standalonescripts/extractgll.py
@@ -67,10 +67,16 @@ class gll:
     def __init__(self, presource, lg, src, imt, trs, filename=None, booklanguage=None):
         self.src = src
         self.imt = imt
-        if booklanguage and booklanguage != "und" :
-            self.language = booklanguage
+        if booklanguage:
+            self.language_iso6393 = booklanguage[0]
+            self.language_glottocode = booklanguage[1]
+            self.language_name = booklanguage[2]
         else:
-            self.language = glottonames.get(lg, ["und",None])[0]
+            self.language_iso6393 = None
+            self.language_name = None
+            self.language_glottocode = glottonames.get(lg, ["und", None])[0]
+            if self.language_glottocode != "und":
+                self.language_name = lg
         self.trs = trs.replace("\\\\"," ").strip()
         if self.trs[0] in STARTINGQUOTE:
             self.trs = self.trs[1:]
@@ -158,16 +164,16 @@ class gll:
         if " not " in self.trs.lower():
             self.polarity = "negative"
 
-langsci_d = {17:'sje', # pite1240
-             66:'ybh', #yakk1236
-             67:'mhl', # mauw1238
-             78:'pmy', # papu1250
-             82:'phl', # phal1254
-             85:'fpe',   #fern1234 (Pichi)
-             118:'mlw', # Molo1266
-             124:'rap',  #  rapa1244
-             212:'tci',  # wara1294 (Komnzo)
-             250:'dar'  #sanz1248          (Iso is not precise here)
+langsci_d = {17: ('sje', "pite1240","Pite Saami"), # pite1240
+             66: ('ybh', "yakk1236", "Yakkha"),#yakk1236
+             67: ('mhl', "mauw1238","Mauwake"),# mauw1238
+             78: ('pmy', "papu1250","Papuan Malay"),# papu1250
+             82: ('phl', "phal1254","Palula"),# phal1254
+             85: ('fpe', "fern1234","Pichi"),  #fern1234 (Pichi)
+             118: ('mlw',"molo1266", "Moloko"),# Molo1266
+             124: ('rap',"rapa1244", "Rapa Nui"), #  rapa1244
+             212: ('tci',"wara1294", "Komnzo"), # wara1294 (Komnzo)
+             250: ('dar', "sanz1248", "Sanzhi Dargwa"), #sanz1248          (Iso is not precise here)
              }
 
 def langsciextract(directory):
@@ -175,7 +181,7 @@ def langsciextract(directory):
     books = glob.glob(f"{directory}/*")
     for book in books:
         book_ID = int(book.split("/")[-1])
-        language = langsci_d.get(int(book_ID), "und")
+        booklanguage = langsci_d.get(int(book_ID), False)
         glossesd = defaultdict(int)
         excludechars = ".\\}{=~:/"
         files = glob.glob(f"{directory}/{book_ID}/chapters/*tex")
@@ -192,7 +198,7 @@ def langsciextract(directory):
             #print(f"  {len(glls)}")
             for g in glls:
                 try:
-                    thisgll = gll(*g, filename=filename, booklanguage=language)
+                    thisgll = gll(*g, filename=filename, booklanguage=booklanguage)
                 except AssertionError:
                     continue
                 examples.append(thisgll)
@@ -204,7 +210,7 @@ def langsciextract(directory):
                 example_block_nif_label = wordstring
                 words_nif_label =  wordstring
                 gloss_language_id = "eng"
-                vernacular_language_id = language
+                vernacular_language_id = booklanguage
                 words = thisgll.srcwordsbare
                 wordglosses = thisgll.imtwordsbare
 
