74c7963adc8421dfa0d14e4d34aa4a17485a0bbe
glottotopia <sebastian.nordhoff@glottotopia.de>
2022-08-17 16:17:35 +0200

    generate proquest xml from yaml

---
+++ langsci/standalonescripts/generate_proquest.py
@@ -0,0 +1,198 @@
+import  yaml
+import sys
+
+metadata = yaml.load(open('metadata.yaml','r'))
+book_DOI = sys.argv[1]
+
+today = "2022-08-08" #FIXME
+publication_date = "1970-01-01" #FIXME
+isbn_digital =  "978"
+issn =  "1234-4321"
+metalanguage = "eng"
+
+authorrolecode = "A01"
+editorrolecode = "B01"
+
+creatortemplate=u"""
+        <Contributor>
+        <SequenceNumber>%s</SequenceNumber>
+        <ContributorRole>%s</ContributorRole>
+        <NamesBeforeKey>%s</NamesBeforeKey>
+        <KeyNames>%s</KeyNames>
+        <BiographicalNote>%s</BiographicalNote>
+      </Contributor>
+
+"""
+
+
+authorstring = ''
+editorstring = ''
+
+try:
+    role = authorrolecode
+    authorstring = "\n".join([creatortemplate % (i+1,role,creator[0],creator[1],creator[2]) for i, creator in enumerate(metadata["creators"]["authors"]) ])
+except TypeError:
+    pass
+try:
+    role = editorrolecode
+    editorstring = [creatortemplate % (i+1,role,creator[0],creator[1],creator[2]) for i, creator in enumerate(metadata["creators"]["editors"]) ]
+except TypeError:
+    pass
+
+creatorstring = authorstring + editorstring
+
+assert creatorstring != ''
+
+
+
+
+
+proquest_template = f"""<?xml version="1.0"?>
+<ONIXMessage release="3.0">
+  <Header>
+    <Sender>
+      <SenderIdentifier>
+        <SenderIDType>05</SenderIDType>
+        <IDValue>978-3-98554</IDValue>
+      </SenderIdentifier>
+      <SenderName>Language Science Press</SenderName>
+      <ContactName>Sebastian Nordhoff</ContactName>
+      <EmailAddress>support@langsci-press.org</EmailAddress>
+    </Sender>
+    <Addressee>
+      <AddresseeName>ProQuest</AddresseeName>
+    </Addressee>
+    <SentDateTime>{today}</SentDateTime>
+  </Header>
+  <Product>
+    <RecordReference>langsci-press.org/{metadata['bookid']}</RecordReference>
+    <NotificationType>03</NotificationType>
+    <RecordSourceType>01</RecordSourceType>
+    <RecordSourceIdentifier>
+      <RecordSourceIDType>05</RecordSourceIDType>
+      <IDValue>978-3-98554</IDValue>
+    </RecordSourceIdentifier>
+    <RecordSourceName>Language Science Press</RecordSourceName>
+    <ProductIdentifier>
+      <ProductIDType>06</ProductIDType>
+      <IDValue>{book_DOI}</IDValue>
+    </ProductIdentifier>
+    <ProductIdentifier>
+      <ProductIDType>15</ProductIDType>
+      <IDValue>{isbn_digital}</IDValue>
+    </ProductIdentifier>
+    <DescriptiveDetail>
+      <!-- P.3 -->
+      <ProductComposition>00</ProductComposition>
+      <ProductForm>ED</ProductForm>
+      <!-- <EpubType>002</EpubType> -->
+      <Measure>
+        <MeasureType>01</MeasureType>
+        <Measurement>24.00</Measurement>
+        <MeasureUnitCode>cm</MeasureUnitCode>
+      </Measure>
+      <Measure>
+        <MeasureType>02</MeasureType>
+        <Measurement>17.00</Measurement>
+        <MeasureUnitCode>cm</MeasureUnitCode>
+      </Measure>
+      <Collection>
+        <CollectionType>10</CollectionType>
+        <CollectionIdentifier>
+          <CollectionIDType>02</CollectionIDType>
+          <IDValue>{issn}</IDValue>
+        </CollectionIdentifier>
+        <CollectionSequence>
+          <CollectionSequenceType>02</CollectionSequenceType>
+          <CollectionSequenceNumber>9</CollectionSequenceNumber>
+        </CollectionSequence>
+      </Collection>
+      <TitleDetail>
+        <TitleType>01</TitleType>
+        <TitleElement>
+          <TitleElementLevel>01</TitleElementLevel>
+          <TitleText textcase="01">{metadata['title']}</TitleText>
+          <Subtitle>{metadata['booksubtitle']}</Subtitle>
+        </TitleElement>
+        <TitleElement>
+          <TitleElementLevel>02</TitleElementLevel>
+          <TitleText textcase="02">{metadata['series']}</TitleText>
+        </TitleElement>
+      </TitleDetail>
+      {creatorstring}
+      <EditionNumber>1</EditionNumber>
+      <Language>
+        <LanguageRole>01</LanguageRole>
+        <LanguageCode>{metalanguage}</LanguageCode>
+      </Language>
+      <Subject>
+        <MainSubject />
+        <SubjectSchemeIdentifier>10</SubjectSchemeIdentifier>
+        <SubjectSchemeVersion>2009</SubjectSchemeVersion>
+        <SubjectCode>{metadata['scheme'][1]}</SubjectCode>
+      </Subject>
+      <AudienceCode>06</AudienceCode>
+      <EpubLicense>
+        <EpubLicenseExpression>
+          <EpubLicenseExpressionLink>https://creativecommons.org/licenses/by/4.0</EpubLicenseExpressionLink>
+        </EpubLicenseExpression>
+      </EpubLicense>
+    </DescriptiveDetail>
+    <CollateralDetail>
+      <TextContent>
+        <TextType>03</TextType>
+        <ContentAudience>00</ContentAudience>
+        <Text textformat="05">
+          {metadata['blurb']}
+        </Text>
+      </TextContent>
+      <SupportingResource>
+        <ResourceContentType>01</ResourceContentType>
+        <ContentAudience>00</ContentAudience>
+        <ResourceMode>03</ResourceMode>
+        <ResourceVersion>
+          <ResourceForm>02</ResourceForm>
+          <ResourceVersionFeature>
+            <ResourceVersionFeatureType>01</ResourceVersionFeatureType>
+            <FeatureValue>D503</FeatureValue>
+          </ResourceVersionFeature>
+          <ResourceLink>https://langsci-press.org/$$$call$$$/submission/cover/cover?submissionId={metadata['bookid']}</ResourceLink>
+        </ResourceVersion>
+      </SupportingResource>
+      <TextContent>
+        <TextType>47</TextType>
+        <Text>This text is licenced as CC-BY 4.0</Text>
+      </TextContent>
+    </CollateralDetail>
+    <PublishingDetail>
+      <Publisher>
+        <PublishingRole>01</PublishingRole>
+        <PublisherName>Language Science Press</PublisherName>
+        <Website>
+          <WebsiteRole>01</WebsiteRole>
+          <WebsiteLink>https://www.langsci-press.org</WebsiteLink>
+        </Website>
+      </Publisher>
+      <CityOfPublication>Berlin</CityOfPublication>
+      <CountryOfPublication>DE</CountryOfPublication>
+      <PublishingStatus>04</PublishingStatus>
+      <PublishingDate>
+        <PublishingDateRole>01</PublishingDateRole>
+        <Date dateformat="00">{publication_date}</Date>
+      </PublishingDate>
+    </PublishingDetail>
+    <UnpricedItemType>01</UnpricedItemType>
+    <othertext>
+            <d102>47</d102>
+            <d103>06</d103>
+            <d104>Der Titel ist Open Access unter der Creative Commons Lizenz BY 4.0</d104>
+  </othertext>
+    <othertext>
+            <d102>46</d102>
+            <d103>01</d103>
+            <d104>https://creativecommons.org/licenses/by/4.0/</d104>
+  </othertext>
+  </Product>
+</ONIXMessage>"""
+
+print(proquest_template)
