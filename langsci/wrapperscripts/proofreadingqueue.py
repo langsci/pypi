c2f143589c8684a946969fedd879234f2763705e
glottotopia <sebastian.nordhoff@glottotopia.de>
2025-02-14 10:43:23 +0100

    accept list as input

--- langsci/wrapperscripts/proofreadingqueue.py
+++ langsci/wrapperscripts/proofreadingqueue.py
@@ -23,17 +23,16 @@ except ImportError:
         biosketches2names,
     )
 
-ID = sys.argv[1]
-soup = get_soup(ID)
-citeinfo = get_citeinfo(soup)
-title = citeinfo["title"].strip()
-edited = citeinfo["ed"] or ""
-creators = citeinfo["creators"]
-edited_by = ""
-if edited:
-    edited_by = "edited "
 
-
-template = f"""<li class="show"><a href="/catalog/book/{ID}"><img style="width: 200px !important;" src="/$$$call$$$/submission/cover/cover?submissionId={ID}" alt="cover for {title}" /><br />{title} {edited_by} by {creators} </a></li>"""
-
-print(template)
+IDs = sys.argv[1:]
+for ID in IDs:
+    soup = get_soup(ID)
+    citeinfo = get_citeinfo(soup)
+    title = citeinfo["title"].strip()
+    edited = citeinfo["ed"] or ""
+    creators = citeinfo["creators"]
+    edited_by = ""
+    if edited:
+        edited_by = "edited "
+    template = f"""<li class="show"><a href="/catalog/book/{ID}"><img style="width: 200px !important;" src="/$$$call$$$/submission/cover/cover?submissionId={ID}" alt="cover for {title}" /><br />{title} {edited_by} by {creators} </a></li>"""
+    print(template)
